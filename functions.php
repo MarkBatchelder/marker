cae7d70fdea5146d5e82d3e9d85070736ddbf98f
Mark Batchelder <code@wpaddiction.com>
2015-11-11 03:40:28 -0800

    Added new JS file to functions.

--- functions.php
+++ functions.php
@@ -12,6 +12,8 @@
  */
 function marker_scripts() {
 	wp_enqueue_style( 'marker-style', get_stylesheet_uri() );
+    
+    wp_enqueue_script( 'marker-navigation', get_template_directory_uri() . '/js/responsive-nav.js', array(), '1.0.39', true );
 
 	wp_enqueue_script( 'marker-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
